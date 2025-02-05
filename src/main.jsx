692dd09de9a805012e670b3b17f77d8c1024e84d
Sreelakshmi P S <sreelakshmips32@gmail.com>
2025-02-05 10:26:36 +0530

    add gtm

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import TagManager from "react-gtm-module";
 
 const GA_MEASUREMENT_ID = "G-RJ449EGX7V"; // Replace with your GA4 Measurement ID
+const GTM_ID = "GTM-WLZ2BXK8"; // Replace with your actual GTM ID
 
 const Main = () => {
   useEffect(() => {
+    // Initialize Google Tag Manager
+    TagManager.initialize({ gtmId: GTM_ID });
+
     // Add Google Analytics script dynamically
     const script = document.createElement("script");
     script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
